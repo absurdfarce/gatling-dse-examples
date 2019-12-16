4c5fb456bb0b01d63969d4a9d7897ffa113fe64c
Bret McGuire <bret.mcguire@datastax.com>
2019-12-16 16:18:46 -0600

    A few minor tweaks, doc and import fixes

--- src/main/scala/com/datastax/gatling/MoreDseCqlStatements.scala
+++ src/main/scala/com/datastax/gatling/MoreDseCqlStatements.scala
@@ -1,10 +1,20 @@
 package com.datastax.gatling
 
 import com.datastax.driver.core.{SimpleStatement => SimpleS}
-import com.datastax.gatling.plugin.model.{DseCqlAttributes, DseCqlAttributesBuilder, DseCqlSimpleStatement, DseCqlStatement}
+import com.datastax.gatling.plugin.model.{DseCqlAttributes, DseCqlAttributesBuilder, DseCqlStatement}
 import io.gatling.commons.validation._
 import io.gatling.core.session._
 
+/**
+ * Working around a small problem with the sample scenario.  We want a query operation after insert to confirm that
+ * we have a unique value with a specific number, but this op is heavily dependent upon the random int given us by the
+ * feeder.  We don't have a way to access session vals within SimpleStatements via the existing plugin API (or even
+ * the recent changes) so we implement a workaround.
+ *
+ * Code below is designed to retrieve a stored SimpleStatement from the Gatling session.  This is very much a hack
+ * since (among other reasons) we wind up with a attributes object which doesn't have a valid Statement subclass for
+ * most of it's life... but it work for what we need.
+ */
 object MoreDseCqlStatements {
   def simpleStatementFromSession(tag:String, key:String): DseCqlAttributesBuilder = {
     DseCqlAttributesBuilder(
